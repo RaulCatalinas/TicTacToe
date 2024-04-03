bad6448d5dbe257ba53c9c2b17ed3bca84ab5947
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2024-04-03 20:48:28 +0200

    chore(Root): BoadLayout.jsx updated

--- src/layouts/BoardLayout.jsx
+++ src/layouts/BoardLayout.jsx
@@ -3,7 +3,7 @@ import styles from '@/css/board.module.css'
 export default function BoardLayout({ children }) {
   return (
     <div className={styles.board}>
-      <h1>TicTacToe</h1>
+      <h1>3 En Raya</h1>
       {children}
     </div>
   )
